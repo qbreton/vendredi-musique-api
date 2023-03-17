5dcd84ca555bb1bce6fc3f9285d7c73fe8e7cbc5
qbreton <qbreton.dev@gmail.com>
2023-03-17 09:47:23 +0100

    Login + firebase

--- index.js
+++ index.js
@@ -8,12 +8,28 @@ const port = process.env.PORT || 3001;
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { expressjwt } = require('express-jwt');
+const { compareSync } = require('bcryptjs');
 require('dotenv').config();
+const firebase = require('firebase-admin');
+const firebaseAccount = JSON.parse(process.env.FIREBASE);
+firebase.initializeApp({
+  credential: firebase.credential.cert(firebaseAccount)
+});
+const db = firebase.firestore();
 
-function drawWinner() {
-  // lire le fichier names.json
-  const filePath = path.join(__dirname, 'names.json');
-  const names = JSON.parse(fs.readFileSync(filePath));
+async function getFirebaseDoc() {
+  const doc = db.collection('names').doc('names');
+  const docData = await doc.get();
+
+  if (docData.exists) {
+    return docData.data();
+  }
+  return null;
+}
+
+async function drawWinner() {
+  const names = await getFirebaseDoc();
+  const doc = db.collection('names').doc('names');
 
   // choisir un nom au hasard parmi ceux qui n'ont pas encore été tirés
   const notDrawn = names.notDrawn;
@@ -28,29 +44,20 @@ function drawWinner() {
   drawn.push(winner);
 
   // écrire le fichier names.json mis à jour
-  const data = JSON.stringify(names, null, 2);
-  fs.writeFileSync(filePath, data);
+  await doc.set({drawn, notDrawn});
 
   return names;
 }
 
-function getDrawnAndNotDrawn() {
-    // lire le fichier names.json
-    const filePath = path.join(__dirname, 'names.json');
-    const names = JSON.parse(fs.readFileSync(filePath));
-  
-    // récupérer les tableaux "notDrawn" et "drawn"
-    const notDrawn = names.notDrawn;
-    const drawn = names.drawn;
-  
-    // retourner les tableaux "notDrawn" et "drawn"
-    return { notDrawn, drawn };
+async function getDrawnAndNotDrawn() {
+    const names = await getFirebaseDoc();
+    return names;
 }
 
-function addName(name) {
+async function addName(name) {
     // lire le fichier names.json
-    const filePath = path.join(__dirname, 'names.json');
-    const names = JSON.parse(fs.readFileSync(filePath));
+    const names = await getFirebaseDoc();
+    const doc = db.collection('names').doc('names');
 
     // vérifier si le nom existe déjà
     const existsCondition = (person) => person.name === name;
@@ -64,27 +71,27 @@ function addName(name) {
     names.notDrawn.push({name, dates: []});
 
     // écrire les données dans le fichier names.json
-    fs.writeFileSync(filePath, JSON.stringify(names));
+    await doc.set(names);
 
     return { notDrawn: names.notDrawn, drawn: names.drawn };
 }
 
-function resetNames() {
+async function resetNames() {
   // lire le fichier names.json
-  const filePath = path.join(__dirname, 'names.json');
-  const names = JSON.parse(fs.readFileSync(filePath));
+  const names = await getFirebaseDoc();
+  const doc = db.collection('names').doc('names');
   const concat = (...arrays) => [].concat(...arrays.filter(Array.isArray));
 
   const newList = { notDrawn: concat(names.notDrawn, names.drawn), drawn: []};
   // écrire les données dans le fichier names.json
-  fs.writeFileSync(filePath, JSON.stringify(newList));
+  await doc.set(newList);
 
   return newList;
 }
 
-function deleteNames(name) {
-  const filePath = path.join(__dirname, 'names.json');
-  const names = JSON.parse(fs.readFileSync(filePath));
+async function deleteNames(name) {
+  const names = await getFirebaseDoc();
+  const doc = db.collection('names').doc('names');
 
   const notDrawn = names.notDrawn.filter((n) => n.name !== name);
   const drawn = names.drawn.filter((n) => n.name !== name);
@@ -92,17 +99,17 @@ function deleteNames(name) {
   names.notDrawn = notDrawn;
   names.drawn = drawn;
 
-  fs.writeFileSync(filePath, JSON.stringify(names));
+  await doc.set(names);
   
   return names;
 }
 
-function undo(name) {
+async function undo(name) {
   if (name === "") { return; }
-  // lire le fichier names.json
-  const filePath = path.join(__dirname, 'names.json');
-  const names = JSON.parse(fs.readFileSync(filePath));
 
+  const names = await getFirebaseDoc();
+  const doc = db.collection('names').doc('names');
+  
   names.drawn.map((person, index) => {
     if (person.name === name && person.dates.length > 0) {
       person.dates.pop();
@@ -111,8 +118,8 @@ function undo(name) {
     }
   });
 
-  // écrire les données dans le fichier names.json
-  fs.writeFileSync(filePath, JSON.stringify(names));
+  // écrire le fichier names.json mis à jour
+  await doc.set(names);
 
   return names;
 }
@@ -125,42 +132,44 @@ const corsOptions = {
 app.use(cors());
 
 app.use('/draw', expressjwt({secret : process.env.JWT_SECRET, algorithms: ['HS256']}));
-app.get('/draw', (req, res) => {
-  const list = drawWinner();
+app.get('/draw', async (req, res) => {
+  const list = await drawWinner();
   res.json(list);
 });
 
 // We block access to post names for unauthorized users
 app.use('/names', expressjwt({secret : process.env.JWT_SECRET, algorithms: ['HS256']}).unless({ method: ['GET'] }));
-app.get('/names', (req, res) => {
-    const { notDrawn, drawn } = getDrawnAndNotDrawn();
-    res.json({ notDrawn, drawn });
+app.get('/names', async (req, res) => {
+    const names = await getDrawnAndNotDrawn();
+    res.json(names);
 });
 
-app.post('/names', jsonParser, (req, res) => {
+app.post('/names', jsonParser, async (req, res) => {
     const { name } = req.body;
     try {
-      const { notDrawn, drawn } = addName(name);
-      res.json({ notDrawn, drawn });
+      const names = await addName(name);
+      res.json(names);
     } catch (err) {
       res.status(400).json({ message: err.message });
     }
 });
 
 app.use('/names/:name', expressjwt({secret : process.env.JWT_SECRET, algorithms: ['HS256']}));
-app.delete('/names/:name', (req, res) => {
-  res.json(deleteNames(req.params.name))
+app.delete('/names/:name', async (req, res) => {
+  const names = await deleteNames(req.params.name);
+  res.json(names);
 });
 
 app.use('/:name/undo', expressjwt({secret : process.env.JWT_SECRET, algorithms: ['HS256']}));
-app.post('/:name/undo', (req, res) => {
-  res.json(undo(req.params.name))
+app.post('/:name/undo', async(req, res) => {
+  const names = await undo(req.params.name);
+  res.json(names);
 });
 
 app.use('/reset', expressjwt({secret : process.env.JWT_SECRET, algorithms: ['HS256']}));
-app.post('/reset', (req, res) => {
-  const { notDrawn, drawn } = resetNames();
-  res.json({ notDrawn, drawn });
+app.post('/reset', async (req, res) => {
+  const names = await resetNames();
+  res.json(names);
 });
 
 app.post('/login', jsonParser, (req, res) => {
@@ -168,12 +177,12 @@ app.post('/login', jsonParser, (req, res) => {
   const { username, password } = req.body;
 
   // Vérifier si l'utilisateur est un admin
-  if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
+  if (username === process.env.ADMIN_USERNAME && compareSync(password, process.env.ADMIN_PASSWORD)) {
       // Si les identifiants sont valides, générer un token d'authentification
       const token = jwt.sign({ username: username }, process.env.JWT_SECRET);
 
       // Retourner le token d'authentification
-      res.status(200).json({ token: token });
+      res.status(200).json({ token });
   } else {
       // Sinon, retourner un message d'erreur
       res.status(401).json({ message: "Invalid credentials" });
